84684325207eecc777ba4d7a3f368868d4e58bd4
mirakelsvampen <ben@kooijman.se>
2020-12-27 18:13:56 +0100

    lolcat tutorial done

--- tinyProjects/clock/script.js
+++ tinyProjects/clock/script.js
@@ -1,31 +1,23 @@
 class Clock {
     constructor() {
+        this.partytime;
+        this.timeOfDay;
+
         this.wakeup = 6;
+        this.morning = 7;
         this.noon = 12;
         this.lunchtime = 12;
         this.naptime = this.lunchtime +2;
         this.evening = 18;
-        this.dinner = 18;
     }
 
     showCurrentTime(){
         clock = document.getElementById("clock");
-
-        let timeOfDay;
+        
         let time = new Date();
         let hour = time.getHours();
         let minute = time.getMinutes();
         let seconds = time.getSeconds();
-    
-        if (hour >= this.evening){
-            timeOfDay = "evening";
-        }
-        else if (hour >= this.noon){
-            timeOfDay = "noon";
-        }
-        else if (hour > this.wakeup){
-            timeOfDay = "morning";
-        }
 
         if (minute < 10){
             minute = "0" + minute;
@@ -35,14 +27,85 @@ class Clock {
             seconds = "0" + seconds;
         }
     
-        const concatTime = "Its " + timeOfDay + " and time is: " + hour + ":" + minute + ":" + seconds;
+        const concatTime = hour + ":" + minute + ":" + seconds;
     
-        clock.innerHTML = concatTime;
+        clock.innerHTML =  concatTime ;
+    }
+
+    updateClock(){
+        let timeImage = document.getElementById("timeImage");
+        let timeMessage = document.getElementById("clockMessage");
+        let time = new Date().getHours();
+        let message;
+        let image = "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/08/normalTime.jpg";
+        
+        if (time == this.partytime){
+            image = "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/08/partyTime.jpg";
+            console.log("It is now partytime");
+            message = "PARTYTIME";
+        }
+        else if (time == this.wakeup){
+            image = "https://vetstreet-brightspot.s3.amazonaws.com/8f/e2/f39c0f3741849b404692aaf33735/cat-on-bed-istock-86906259-large-335.jpg";
+            console.log("it is now time to wakeup");
+            message = "Wake up!";
+        }
+        else if (time == this.lunchtime){
+            image = "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/cat2.jpg";
+            console.log("It is now time to eat some lunch");
+            message = "Time to eat some lunch!";
+        }
+        else if (time == this.naptime){
+            image = "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/cat3.jpg";
+            console.log("It is now time to take a nap");
+            message = "Let's take a nap...";
+        }
+        else if (time < this.noon){
+            image = "https://news3lv.com/resources/media/6ecd9a16-896b-45db-a687-75919fad4065-large16x9_CatCafe.jpg?1565220634335"
+            console.log("It is morning now");
+            message = "Good morning!";
+        }
+        else if (time < this.evening){
+            image = "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/08/normalTime.jpg";
+            console.log("It is now evening");
+            message = "Good evening.";
+        }
+        else {
+            image = "https://cdn.pixabay.com/photo/2017/05/31/09/54/cat-2359779_960_720.jpg"
+            console.log("it is now afternoon");
+            message = "Good afternoon.";
+        }
+        console.log(time + ""+ this.naptime);
+        timeImage.src = image;
+        timeMessage.innerHTML = message;
+        this.showCurrentTime();
     }
 
 }
 
 const newClock = new Clock();
+newClock.updateClock();
+
+let wakeUpTimeSelector = document.getElementById("wakeUpTimeSelector");
+let wakeUpEvent = function(){
+    newClock.wakeup = wakeUpTimeSelector.value;
+    console.log("wakeUp time has changed to" + wakeUpTimeSelector.value);
+}
+wakeUpTimeSelector.addEventListener("change", wakeUpEvent);
+
+let lunchTimeSelector = document.getElementById("lunchTimeSelector");
+let lunchEvent = function(){
+    newClock.lunchtime = lunchTimeSelector.value;
+    console.log("lunchTime time has changed to" + lunchTimeSelector.value);
+}
+lunchTimeSelector.addEventListener("change", lunchEvent);
+
+let napTimeSelector = document.getElementById("napTimeSelector");
+let napTimeEvent = function(){
+    newClock.naptime = napTimeSelector.value;
+    console.log("napTime time has changed to" + napTimeSelector.value);
+}
+napTimeSelector.addEventListener("change", napTimeEvent);
+
 setInterval(function (){
-    newClock.showCurrentTime();
-}, 1);
+    newClock.updateClock();
+}, 1000);
